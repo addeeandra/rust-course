59c9ed2eb36c735708dd0e23a7c1aee8e389f270
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 19:40:25 +0700

    basic loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let n = 32;
+    let mut n = 0;
 
-    // n < 30, n > 30, n != 30
-    if n > 30 {
-        println!("The number is more than 30");
-    } else if n < 30 {
-        println!("The number is less than 30");
-    } else {
-        println!("The number is 30");
+    loop {
+        n += 1;
+
+        if n == 7 {
+            continue;
+        }
+
+        if n > 10 {
+            break;
+        }
+
+        println!("The value of n is {}", n);
     }
 }
