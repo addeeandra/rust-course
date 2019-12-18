3957cc2cfe4e961366bf2b8c20a2ae3ba705b42a
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 19:32:45 +0700

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 45;
+    let mut y = 35;
+
+    println!("The value of x is {}", x);
+    println!("The value of y is {}", y);
+
+    // x = 50; // error
+    y = 40;
+
+    println!("The value of y is {}", y);
 }
