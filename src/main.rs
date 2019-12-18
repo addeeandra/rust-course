560091c9d8f3655647d16cbda01f8c3021384910
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 20:10:16 +0700

    references

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 fn main() {
     let mut x = 10;
+    // let xr = &x; // references to x
 
-    {
-        // shadowing
-        let x = 15;
-        println!("x is {}", x);
+    println!("x is {}", x);
+
+    { // only able to borrow 1 mutable references (safety call inside a block)
+        let dom = &mut x; // references to x (mutable)
+        *dom += 1;
+        println!("dom is {}", dom);
     }
 
-    println!("x is {}", x);
-
-    // shadowing
-    let x = "X is a string";
-    println!("x is {}", x);
-
-    // shadowing
-    let x = true;
     println!("x is {}", x);
 }
