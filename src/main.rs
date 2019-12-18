f081d2fc3c7fdc01ea5b5dc304fd935e49eb6fbe
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 19:55:30 +0700

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-const MAXIMUM_NUMBER: u8 = 20;
-
 fn main() {
-    for n in 1..MAXIMUM_NUMBER {
-        println!("{}", n);
-    }
+    let tup1 = (20, 25, 30, 35);
+    let tup2 = (10, 3.4, "Rust", false, (1, 4, 6));
+
+    let tup3 = (45, 6.7, "Computer");
+    let (a, b, c) = tup3;
+
+    println!("{}", tup1.0);
+    println!("{}", tup2.2);
+    println!("{}", (tup2.4).2);
+    println!("a is {}, b is {}, c is {}", a, b, c);
 }
