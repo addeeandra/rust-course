1a25108f0450168c98726d8a7b559bcf2cfecbe7
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 20:21:43 +0700

    pass by references

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-struct Color(u8, u8, u8);
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
 fn main() {
-    let red = Color(255, 0, 0);
-    // let mut red = Color(255, 0, 0);
+    let blue = Color { red: 0, green: 0, blue: 255 };
+    let red = Color { red: 255, green: 0, blue: 0 };
 
-    println!("red is {}, {}, {}", red.0, red.1, red.2);
+    print_color_1(blue);
+    print_color_1(blue); // moved value, gain error
+
+    print_color_2(&red);
+    print_color_2(&red); // not moved, cause the references are passed instead
+}
+
+fn print_color_1(c: Color) {
+    println!("Color - R:{} G:{} B:{}", c.red, c.green, c.blue);
+}
+
+// pass by references to avoid borrowing
+fn print_color_2(c: &Color) {
+    println!("Color - R:{} G:{} B:{}", c.red, c.green, c.blue);
 }
