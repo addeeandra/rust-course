d82f0545650f0ecb797b32b25b794e3084c8fc73
Aditya Chandra <inibukanadit@gmail.com>
2019-12-18 19:37:58 +0700

    if else if and else statements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let x: i64 = 55;
-    let y: u64 = 60; // unsigned integer
-    let f: f32 = 7.6;
-    let b: bool = false;
+    let n = 32;
+
+    // n < 30, n > 30, n != 30
+    if n > 30 {
+        println!("The number is more than 30");
+    } else if n < 30 {
+        println!("The number is less than 30");
+    } else {
+        println!("The number is 30");
+    }
 }
